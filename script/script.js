90bdf532477d703c10dbd795db5c6612b64c6d17
Burak Kumaş <brkkumas@gmail.com>
2023-06-25 21:06:56 +0300

    update

--- script/script.js
+++ script/script.js
@@ -3,3 +3,16 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
   header.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+let menu = document.getElementById("menu-icon");
+
+let navmenu = document.querySelector(".navmenu");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navmenu.classList.toggle("open")
+};
+
+// menu.onClick = () => {
+//   menu.classList.toggle("bx-x");
+// };
